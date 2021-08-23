baf38eebaef405e48612f93c5e7a413058bbd7e6
Tan Yee Jian <yeejian.tan@u.nus.edu>
2021-08-23 16:25:05 +0800

    Automated backup

--- y4s1/cs4212/tut1.py
+++ y4s1/cs4212/tut1.py
@@ -4,17 +4,22 @@ import sys
 
 # We just hard code the following transition table
 trans = {
-    'A':('B', 'C'), # shorthand for {0:'B', 1:'C'}
-    'B':('X', 'D'),
-    'C':('E', 'X'),
-    'D':('B', 'X'),
-    'E':('F', 'X'),
-    'F':('X', 'C')
+    "A": ("B", "C"),  # shorthand for {0:'B', 1:'C'}
+    "B": ("X", "D"),
+    "C": ("E", "X"),
+    "D": ("B", "X"),
+    "E": ("F", "X"),
+    "F": ("X", "C"),
 }
 starting_state = "A"
 ending_states = "A, D, F"
+alphabet = "01"
+
 
 def main():
     state = starting_state
     for line in sys.stdin:
         for char in line:
+            if char not in alphabet:
+                print(f"Invalid character: {char}")
+                exit(1)
